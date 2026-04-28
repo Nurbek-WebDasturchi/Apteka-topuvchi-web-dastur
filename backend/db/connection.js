c0af58bdd875627ecc17cbd1025c292fd1af940b
Nurbek-WebDasturchi <ahmedovnurbek26042005@gmail.com>
2026-04-29 01:22:26 +0500

    db is little bit changed to alive

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,20 +1,13 @@
-// ── MA'LUMOTLAR BAZASIGA ULANISH ──────────────────────────────
 const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || "aptek_db",
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // ← Supabase uchun shart
+    rejectUnauthorized: false,
   },
-  family: 4, // ← IPv6 o'rniga IPv4 majburlash
 });
 
-// Ulanishni tekshirish
 pool.connect((err, client, release) => {
   if (err) {
     console.error("❌ PostgreSQL ulanish xatosi:", err.message);
